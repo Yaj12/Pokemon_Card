539a7527094c3829333faf3c59e28835c0229ec9
9p6b <9p6b@fresnou.org>
2021-12-07 08:16:24 -0800

    Two Pokemon Cards Complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const pokemonController = require('../controllers/pokemonController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', pokemonController.viewAll);
+
 
 module.exports = router;
